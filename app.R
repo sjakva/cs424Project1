0671770e5600c5f07d1daafc4a14bb63028bc4dd
Shoaib Jakvani <sjakva3@uic.edu>
2022-02-12 20:46:25 -0600

    finished as much as possible

--- app.R
+++ app.R
@@ -14,21 +14,33 @@ library(tidyverse)
 # --------------------------------------------------------------
 
 # list of years
-years<-c(2001:2021)
+yearsList <- c(2001:2021)
+stationsList <- c("UIC-Halsted", "O'Hare", "Medical Center")
 
 # read in data-file
 halstedData <- read.table(file="./halsted.tsv", sep="\t", header=TRUE)
+ohareData <- read.table(file="./ohare.tsv", sep="\t", header=TRUE)
+medicalData <- read.table(file="./medical.tsv", sep="\t", header=TRUE)
 
 newDate <- as.Date(halstedData$date, "%m/%d/%Y")
 halstedData$nDate <- newDate
 halstedData$date <-NULL
+newDate <- as.Date(ohareData$date, "%m/%d/%Y")
+ohareData$nDate <- newDate
+ohareData$date <- NULL
+newDate <- as.Date(medicalData$date, "%m/%d/%Y")
+medicalData$nDate <- newDate
+medicalData$date <- NULL
 
-# Entries per day over 2021
-days2021 <- subset(halstedData, year(nDate) == 2021 )
+# # # Entries per day over 2021
+# yearToLookFor <- 2021
+# daysHalsted <- subset(halstedData, year(nDate) == yearToLookFor )
+# daysHalsted <- subset(ohareData, year(nDate) == yearToLookFor )
+# daysMedical2021 <- subset(medicalData, year(nDate) == yearToLookFor )
 
 # look at the data loaded
 # View(halstedData)
-# View(days2021)
+# View(daysHalsted)
 
 
 # Define UI for application
@@ -38,47 +50,68 @@ ui <- dashboardPage(
                    sidebarMenu(
                      id = "tabsMenu",
                      menuItem("Home", tabName = "homeTab", icon = NULL),
-                     menuItem("Compare", tabName = "compareStationsTab", icon = NULL)
+                     menuItem("Compare", tabName = "compareStationsTab", icon = NULL),
+                     menuItem("About", tabName = "aboutMe", icon = NULL)
                      )
                    ),
   dashboardBody(
-    # selectInput("stationInput", "Choose a station", c("UIC-Halsted", "[station]", "[station]")),
-    tabItems(
-      tabItem(tabName = "homeTab",
+    tabItems( 
+      tabItem(tabName = "homeTab", # --------------------------------------------------------------------------------------------
               fluidRow(
                 column(6,
-                       # "row"
+                       # 
                        box( title = "Station Entries Per Year", solidHeader=TRUE, status="success", width=12, background="purple",
                             plotOutput("halstedYears", height=800)
                        )
-                       # dateInput("day2021", "Enter a date:", 
-                       #           format="mm-dd-yy", value="2021-01-01", 
-                       #           min="2021-01-01", max="2021-12-31",
-                       #           startview="month", autoclose=FALSE),
-                       # box( title = "Station Entries Per Year", solidHeader=TRUE, status="success", width=12, background="purple",
-                       #      plotOutput("halstedDays", height=250)
-                       # )
                 ),
                 column(6,
-                       "oharee"
+                       box( title = "Station Entries Per Year", solidHeader=TRUE, status="success", width=12, background="purple",
+                            plotOutput("ohareYears", height=800)
+                       )
                 )
               )
             ),
     
     
-      tabItem(tabName = "compareStationsTab",
+      tabItem(tabName = "compareStationsTab", # ---------------------------------------------------------------------------------
               fluidRow(
                 column(6,
-                       box( title = "Station Entries for 2021", solidHeader=TRUE, status="success", width=12, background="purple",
-                            plotOutput("halstedDays", height=300)
+                       selectInput("yearInput1", "Choose a year", yearsList, selected = 2021),
+                       selectInput("stationInput", "Choose a station", stationsList, selected = "UIC-Halsted"),
+
+                       box( title = "Station Entries for the Year", solidHeader=TRUE, status="success", width=12, background="purple",
+                            plotOutput("halstedDays", height=200)
                        ),
-                       box( title = "Station Entries for 2021 by Month", solidHeader=TRUE, status="success", width=12, background="purple",
-                            plotOutput("halstedMonths", height=300)
+                       box( title = "Station Entries by Month", solidHeader=TRUE, status="success", width=12, background="purple",
+                            plotOutput("halstedMonths", height=200)
+                       ),
+                       box( title = "Station Entries by Day of the Week", solidHeader=TRUE, status="success", width=12, background="purple",
+                            plotOutput("halstedDaysNamed", height=200)
                        )
                 ),
                 column(6,
-                       "bobo"
+                       selectInput("yearInput2", "Choose a year", yearsList, selected = 2021),
+                       selectInput("stationInput", "Choose a station", stationsList, selected = "O'Hare"),
+                       
+                       box( title = "Station Entries for the Year", solidHeader=TRUE, status="success", width=12, background="purple",
+                            plotOutput("ohareDays", height=200)
+                       ),
+                       box( title = "Station Entries by Month", solidHeader=TRUE, status="success", width=12, background="purple",
+                            plotOutput("ohareMonths", height=200)
+                       ),
+                       box( title = "Station Entries by Day of the Week", solidHeader=TRUE, status="success", width=12, background="purple",
+                            plotOutput("ohareDaysNamed", height=200)
+                       )
                 )
+              )
+      ),
+      
+      
+      tabItem(tabName = "aboutMe", # ---------------------------------------------------------------------------------
+              fluidRow(
+                h2("About Page"),
+                p("Data given from CS424 class at UIC during Spring 2022"),
+                p("Author:  Shoaib Jakvani")
               )
       )
     ),
@@ -91,6 +124,18 @@ ui <- dashboardPage(
 # --------------------------------------------------------------
 # Define server logic
 server <- function(input, output) {
+  
+  values <- reactiveValues()
+  # values$yearsToLookFor1 <- selectInput$yearInput1
+  # values$yearsToLookFor2 <- selectInput$yearInput2
+  values$yearsToLookFor1 <- 2021
+  values$yearsToLookFor2 <- 2021
+  
+  # # Entries per day over 2021
+  # yearToLookFor <- 2021
+  daysHalsted <- reactive(subset(halstedData, year(nDate) == isolate(values$yearsToLookFor1) ))
+  daysOhare <- reactive(subset(ohareData, year(nDate) == isolate(values$yearsToLookFor1) ))
+  
 
   # output$map renderPlot({
   #   # ----------------------------------------------------------
@@ -100,27 +145,67 @@ server <- function(input, output) {
   output$halstedYears <- renderPlot({
     # ggplot(noons, aes(x=newDate, y=S2)) + geom_point(color="blue") +  labs(title="Room Temperature in room ???", x="Day", y = "Degrees F") + geom_line()
     ggplot(halstedData, aes(x=year(nDate), y=rides))+
-      labs(x="Years", y="Amount of Entries", title="Entries over the Years")+
+      labs(x="Years", y="Amount of Entries")+
+      geom_bar(stat="identity", fill="palegreen")+
+      theme_dark()
+  })
+  output$ohareYears <- renderPlot({
+    ggplot(ohareData, aes(x=year(nDate), y=rides))+
+      labs(x="Years", y="Amount of Entries")+
       geom_bar(stat="identity", fill="palegreen")+
       theme_dark()
   })
   
+  
+  
   output$halstedDays <- renderPlot({
-    ggplot(days2021, aes(x=nDate, y=rides))+
-      labs(x="Days", y="Amount of Entries", title="Entries per day over 2021")+
+    ggplot(isolate(daysHalsted()), aes(x=nDate, y=rides))+
+      labs(x="Days", y="Amount of Entries")+
       geom_bar(stat="identity", fill="palegreen")+
       theme_dark()
   })
   
   output$halstedMonths <- renderPlot({
-    ggplot(days2021, aes(x=month(nDate, label=TRUE), y=rides))+
-      labs(x="Days", y="Amount of Entries", title="Entries per day over 2021")+
+    ggplot(isolate(daysHalsted()), aes(x=month(nDate, label=TRUE), y=rides))+
+      labs(x="Days", y="Amount of Entries")+
+      geom_bar(stat="identity", fill="palegreen")+
+      theme_dark()
+  })
+  
+  output$halstedDaysNamed <- renderPlot({
+    ggplot(isolate(daysHalsted()), aes(x=wday(nDate, label=TRUE), y=rides))+
+      labs(x="Days", y="Amount of Entries")+
+      geom_bar(stat="identity", fill="palegreen")+
+      theme_dark()
+  })
+  
+  # 
+  
+  output$ohareDays <- renderPlot({
+    ggplot(isolate(daysOhare()), aes(x=nDate, y=rides))+
+      labs(x="Days", y="Amount of Entries")+
+      geom_bar(stat="identity", fill="palegreen")+
+      theme_dark()
+  })
+  
+  output$ohareMonths <- renderPlot({
+    ggplot(isolate(daysOhare()), aes(x=month(nDate, label=TRUE), y=rides))+
+      labs(x="Days", y="Amount of Entries")+
+      geom_bar(stat="identity", fill="palegreen")+
+      theme_dark()
+  })
+  
+  output$ohareDaysNamed <- renderPlot({
+    ggplot(isolate(daysOhare()), aes(x=wday(nDate, label=TRUE), y=rides))+
+      labs(x="Days", y="Amount of Entries")+
       geom_bar(stat="identity", fill="palegreen")+
       theme_dark()
   })
   
   
+  
 }
+
 
 # Run the application 
 shinyApp(ui = ui, server = server)
